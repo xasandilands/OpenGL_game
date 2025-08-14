a12f03ff4d72eab6a04088f3734058d61d0eb1a5
Xavier <njsandilands@gmail.com>
2025-08-14 17:22:27 -0500

    bg color + shader files

---
+++ Game/default.frag
@@ -0,0 +1,8 @@
+#version 330 core
+//frag shader
+out vec4 FragColor;
+
+void main()
+{
+	FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
--- Game/main.cpp
+++ Game/main.cpp
@@ -2,33 +2,45 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+//height and width of the window
 int width = 1000;
 int height = 800;
 
 int main()
 {
+	// Initialize GLFW
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	//initialize the window
     GLFWwindow* window = glfwCreateWindow(width, height, "LearnOpenGL", NULL, NULL);
+	//check if the window was created successfully
     if (window == NULL)
     {
         std::cout << "failed to initialize window" << std::endl;
         glfwTerminate();
         return -1;
     }
+	// Set the current context to the window
     glfwMakeContextCurrent(window);
 
+	//load OpenGL function pointers using GLAD and set the viewport
     gladLoadGL();
     glViewport(0, 0, width, height);
 
-	glfwMakeContextCurrent(window);
+	//while loop to keep the window open
     while (!glfwWindowShouldClose(window))
     {
+		//set the background color and clear the buffers
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		//swap back buffer with front buffer and poll for events
+		glfwSwapBuffers(window);
         glfwPollEvents();
     }
+	//clean up and terminate GLFW
     glfwDestroyWindow(window);
     glfwTerminate();
 	return 0;
