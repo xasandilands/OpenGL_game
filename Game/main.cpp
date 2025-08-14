431b9b606af594f0fd0a21291490e0d4346f71fe
Xavier <njsandilands@gmail.com>
2025-08-14 16:48:41 -0500

    window

--- Game/main.cpp
+++ Game/main.cpp
@@ -2,7 +2,34 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+int width = 1000;
+int height = 800;
+
 int main()
 {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(width, height, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "failed to initialize window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    gladLoadGL();
+    glViewport(0, 0, width, height);
+
+	glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
 	return 0;
 }
