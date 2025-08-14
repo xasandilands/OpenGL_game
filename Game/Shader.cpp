dc408ddc47ccc0ccbf53904250d8fd2c6a22df3d
Xavier <njsandilands@gmail.com>
2025-08-14 17:28:34 -0500

    shader class
    reads through the given shader files, creates the shaders and links the 2 shaders together in one shader program

---
+++ Game/Shader.cpp
@@ -0,0 +1,62 @@
+#include"Shader.h"
+
+// Function to read the content of a file and return it as a string
+std::string get_file_content(const char* filename)
+{
+	std::ifstream in(filename, std::ios::binary);
+	if (in)
+	{
+		std::string content;
+		in.seekg(0, std::ios::end);
+		content.resize(in.tellg());
+		in.seekg(0, std::ios::beg);
+		in.read(&content[0], content.size());
+		in.close();
+		return(content);
+	}
+	throw std::runtime_error(std::string("Failed to open file: ") + filename);
+}
+
+Shader::Shader(const char* vertexFile, const char* fragmentFile)
+{
+	//reads the vertex and fragment shader files and stores contents in strings
+	std::string vertexContents = get_file_content(vertexFile);
+	std::string fragmentContents = get_file_content(fragmentFile);
+
+	//converts strings into character arrays
+	const char* vertexSource = vertexContents.c_str();
+	const char* fragmentSource = fragmentContents.c_str();
+
+	//create vertex and fragment shader objs
+	GLuint vertexShader, fragmentShader;
+	//gets refrence of vertex shader
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//compile the vertex shader
+	glCompileShader(vertexShader);
+
+	//gets reference of fragment shader
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//compile the fragment shader
+	glCompileShader(fragmentShader);
+
+	//create the shader program
+	shadRef = glCreateProgram(); 
+	//links the vertex and fragment shaders to the shader program
+	glLinkProgram(shadRef);
+	
+	//delete the vertex and fragment shaders they are not needed anymore
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+}
+
+void Shader::Activate()
+{
+	//activates the shader program
+	glUseProgram(shadRef);
+}
+
+void Shader::Delete()
+{
+	//deletes the shader program
+	glDeleteProgram(shadRef);
+}
--- Game/Shader.h
+++ Game/Shader.h
@@ -1,1 +1,29 @@
-#pragma once
+#ifndef SHADER_CLASS_H
+#define SHADER_CLASS_H
+
+#include<glad/glad.h>
+#include<string>
+#include<fstream>
+#include<sstream>
+#include<iostream>
+#include<cerrno>
+
+std::string get_file_content(const char* filename);
+
+class Shader
+{
+	public:
+		//reference of shader program
+		GLuint shadRef;
+		//constructor that takes vertex and fragment shader file names to build the shader program
+		Shader(const char* vertexFile, const char* fragmentFile);
+
+		//activate the shader program
+		void Activate();
+		//delete the shader program
+		void Delete();
+
+		void compileErros(unsigned int shader, const char* type);
+};
+
+#endif
